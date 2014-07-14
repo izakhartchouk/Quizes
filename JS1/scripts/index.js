0a90f1d51dbcc773cf06f6f78c6d5299117d0645
IPhantom <ivannn787@gmail.com>
2014-07-14 21:33:51 +0300

    added 2 ways

---
+++ JS1/scripts/index.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    var a = -1, b = 2, c;
+
+    //Way 1//
+    var flag = true;
+    c = !flag * a + flag * b;
+
+    flag = false;
+    c = !flag * a + flag * b;
+
+    //Way 2//
+    var array = [a, b];
+
+    flag = true;
+    c = array[+flag];
+
+    flag = false;
+    c = array[+flag];
+})();
